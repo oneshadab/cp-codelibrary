600ac76acde31b982f97a77a4329a78983fa04ba
fahim1993 <fahim.cross@gmail.com>
2017-07-13 17:28:10 +0600

    Init Headers & Defines

---
+++ Headers & Defines.cpp
@@ -0,0 +1,78 @@
+//#pragma warning (disable: 4786)
+//#pragma comment (linker, "/STACK:0x800000")
+//#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+
+using namespace std;
+
+#define EPS 1e-9
+#define FOR(i,N) FORR(i, 0, N)
+#define FORR(i,a,b) FOTR(i, a, b, 1)
+#define FOTR(i,a,b,c) for(int i=(a);i<(b);i+=(c))
+#define FOREACH(it, x) for(__typeof__((x).begin()) it=(x).begin();it!=(x).end();it++)
+#define MEM(a,x) memset(a,x,sizeof(a))
+#define BCHK(a,x) (((a)>>(x))&1)
+#define BSET(a,x) ((a)|(1<<(x)))
+#define BCLR(a,x) ((a)&(~(1<<(x))))
+#define BTGL(a,x) ((a)^(1<<(x)))
+#define FMT(...) (sprintf(CRTBUFF, __VA_ARGS__)?CRTBUFF:0)
+#define read() freopen("input.txt","r",stdin)
+#define write() freopen("output.txt","w",stdout)
+#define cpp_io() {ios_base::sync_with_stdio(false);cin.tie(NULL);}
+#define BUFFSIZE 30000
+#define INF 1000000000
+#define MAX 3000010
+#define MOD 1000000007
+#define pb push_back
+#define mkpr make_pair
+#define pii pair<int, int>
+#define PI acos(-1)
+
+char CRTBUFF[BUFFSIZE];
+#define dbg(args...) {cout<<"-->";debugger::call(#args,args);cout<<"\n";}
+struct debugger {
+    static void call(const char* it) {}
+    template<typename T, typename ... aT>
+    static void call(const char* it, T a, aT... rest) {
+        string b;
+        for (; *it&&*it != ','; ++it) if (*it != ' ')b += *it;
+        cout << b << "=" << a << " ";
+        call(++it, rest...);
+    }
+};
+
+template<class T > T _abs(T n) { return (n < 0 ? -n : n); }
+template<class T > T _max(T a, T b) { return (!(a < b) ? a : b);}
+template<class T > T _min(T a, T b) { return (a < b ? a : b); }
+template<class T > T sq(T x) { return x * x; }
+
+const double V_PI = 3.141592653589793;
+const double V_e = 2.718281828459045;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cout<<"HI\n";
+}
