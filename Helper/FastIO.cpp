99d029e905aa7e3b80e8d94458236e6a9dc744c8
fahim1993 <fahim.cross@gmail.com>
2017-07-30 19:08:56 +0600

    refactor

--- Helper/FastIO.cpp
+++ Helper/FastIO.cpp
@@ -1,87 +1,94 @@
 #include <bits/stdc++.h>
 
-#define tpname typename
-
 using namespace std;
 
-struct FastIO{
+struct FastIO {
     static const int MaxSize = 8192;
-    char in[MaxSize+10], out[MaxSize], buffer[MaxSize];
+    char in[MaxSize + 10], out[MaxSize], buffer[MaxSize];
     int len, pos, cnt;
-    char nextChar(){
-        if(pos == len) len = fread(in, sizeof(char), MaxSize, stdin), pos = 0;
-        if(pos == len) return EOF;
+
+    char nextChar() {
+        if (pos == len) len = fread(in, sizeof(char), MaxSize, stdin), pos = 0;
+        if (pos == len) return EOF;
         return in[pos++];
     }
-    void printChar(char ch){
-        if(cnt == MaxSize) flush();
+
+    void printChar(char ch) {
+        if (cnt == MaxSize) flush();
         out[cnt++] = ch;
     }
-    void flush(){
-        if(cnt) fwrite(out, sizeof(char), cnt, stdout);
+
+    void flush() {
+        if (cnt) fwrite(out, sizeof(char), cnt, stdout);
         cnt = 0;
     }
-    ~FastIO(){
+
+    ~FastIO() {
         flush();
     }
+
     /* For Numbers */
-    template<tpname T, tpname = tpname enable_if<is_integral<T>::value,T>::type>
-    FastIO& operator>>(T& num){
+    template<typename T,
+            typename = typename enable_if<is_integral<T>::value, T>::type>
+    FastIO &operator>>(T &num) {
         num = 0;
         char ch = nextChar(), neg = false;
-        while(!isdigit(ch) && ch != '-' && ch != '+') ch = nextChar();
-        if(ch == '-' || ch == '+') neg = (ch == '-'), ch = nextChar();
-        while(isdigit(ch)) num = num*10+ch-'0', ch = nextChar();
-        if(neg) num = -num;
+        while (!isdigit(ch) && ch != '-' && ch != '+') ch = nextChar();
+        if (ch == '-' || ch == '+') neg = (ch == '-'), ch = nextChar();
+        while (isdigit(ch)) num = num * 10 + ch - '0', ch = nextChar();
+        if (neg) num = -num;
         return *this;
     }
-    template<tpname T, tpname = tpname enable_if<is_integral<T>::value,T>::type>
-    FastIO& operator<<(T num){
+
+    template<typename T,
+            typename = typename enable_if<is_integral<T>::value, T>::type>
+    FastIO &operator<<(T num) {
         int n = 0, neg = false;
-        if(num < 0) neg = true, num = -num;
-        while(num || !n) buffer[n++] = num % 10 + '0', num = num / 10;
-        if(neg)  printChar('-');
-        while(n) printChar(buffer[--n]);
+        if (num < 0) neg = true, num = -num;
+        while (num || !n) buffer[n++] = num % 10 + '0', num = num / 10;
+        if (neg) printChar('-');
+        while (n) printChar(buffer[--n]);
         return *this;
     }
+
     /* For Characters */
-    FastIO& operator>>(char& ch){
+    FastIO &operator>>(char &ch) {
         ch = nextChar();
         return *this;
     }
-    FastIO& operator<<(char ch){
+
+    FastIO &operator<<(char ch) {
         printChar(ch);
         return *this;
     }
+
     /*For Strings */
-    FastIO& operator>>(string& s){
+    FastIO &operator>>(string &s) {
         s.clear();
         char ch = nextChar();
-        while(ch == ' ' || ch == '\n') ch = nextChar();
-        while(ch != ' ' && ch != '\n') s.push_back(ch), ch = nextChar();
+        while (ch == ' ' || ch == '\n') ch = nextChar();
+        while (ch != ' ' && ch != '\n') s.push_back(ch), ch = nextChar();
         return *this;
     }
-    FastIO& operator<<(string s){
-        for(auto ch: s) printChar(ch);
+
+    FastIO &operator<<(string s) {
+        for (auto ch: s) printChar(ch);
         return *this;
     }
     /*For Doubles */
 
 } fio;
 
-
-int main()
-{
+int main() {
     //SPOJ INTEST
-    freopen("input.txt", "r", stdin);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     long long n, k, ans = 0;
     fio >> n >> k;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         int x;
         fio >> x;
-        if(x % k == 0) ans++;
+        if (x % k == 0) ans++;
     }
     fio << ans;
 }
