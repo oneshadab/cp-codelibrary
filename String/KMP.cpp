45faa77a8a93612963dccd1098ac5edcd6388658
fahim1993 <fahim.cross@gmail.com>
2017-07-30 23:18:53 +0600

    refactor

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -2,35 +2,32 @@
 
 using namespace std;
 
-vector<int> kmp(string& str)
-{
+vector<int> kmp(string &str) {
     vector<int> fail;
     fail.push_back(0);
     int pos = 0;
-    for(int i = 1; i < str.length(); i++) {
+    for (int i = 1; i < str.length(); i++) {
         fail.push_back(pos);
-        while(pos > 0 && str[i] != str[pos])
+        while (pos > 0 && str[i] != str[pos])
             pos = fail[pos];
-        if(str[i] == str[pos])
+        if (str[i] == str[pos])
             pos += 1;
     }
     fail.push_back(pos);
     return fail;
 }
 
-
-int main()
-{
-    for(; ;) {
+int main() {
+    for (;;) {
         string str;
         cin >> str;
-        if(str == ".") break;
+        if (str == ".") break;
         int len = str.length();
         vector<int> fail = kmp(str);
         int cnt = fail.back();
         int left = len - cnt;
         int ans = 1;
-        if(len % left == 0) ans = len / left;
+        if (len % left == 0) ans = len / left;
         cout << ans << "\n";
     }
 }
